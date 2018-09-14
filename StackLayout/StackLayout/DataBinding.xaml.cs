290b8426e5589f3e5f6c7fd96336025f08bc1289
Jeffrey <jojober@yahoo.com>
2018-09-14 10:53:52 +0800

    first commit

---
+++ StackLayout/StackLayout/DataBinding.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StackLayout
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DataBinding : ContentPage
+	{
+	   public string Quotes { get; set; }
+
+		public DataBinding ()
+		{
+		    Quotes = "I am so clever that sometimes I don\'t understand a single word of what I am saying.";
+            InitializeComponent();
+
+		    Quote.Text = Quotes;
+
+		}
+
+	}
+}
---
+++ StackLayout/StackLayout/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StackLayout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void AboutClick(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new exercise2());
+
+        }
+
+        private async void CalcMe(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+
+        private async void ShowDataBinding(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DataBinding());
+        }
+    }
+}
